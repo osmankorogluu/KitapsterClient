879c17e68d71097503b992e735d06d514dd3049a
osmankorogluu <korogluosman85@gmail.com>
2023-07-20 23:30:36 +0300

    AdminUi

--- KitapsterClient/src/app/app.module.ts
+++ KitapsterClient/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { UiModule } from './ui/ui.module';
+import { ComponentsModule } from "./admin/layout/components/components.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AdminModule,
+        UiModule,
+        ComponentsModule
+    ]
 })
 export class AppModule { }
